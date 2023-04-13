d5fe498f8514932db8b4b45965ac16b88995da8e
hyqzxc <129652668+hyqzxc@users.noreply.github.com>
2023-04-13 18:33:20 +0800

    Update 八进制到十进制cpp.cpp

--- 八进制到十进制cpp.cpp
+++ 八进制到十进制cpp.cpp
@@ -1,30 +1,21 @@
-#include<stdio.h>
-#include<string.h>
-int main()
-{
-	char s[6];		int n, i;
-	while (scanf("%s", s) != EOF)
-	{
-		if (s[1] == 0)
-		{
-			break;
-		}
-		else
-		{
-			n = strlen(s);
-			int a[6], k = 0;
-			for (i = 0; i < 6; i++) {
-				if (s[i] >= '0' && s[i] <= '7') {
-					a[k++] = s[i] - '0';
-				}
-			}
-			int sum = 0;
-			for (i = 0; i < k; i++) {
-				sum += sum * 7 + a[i];
-			}
-			printf("%d", sum);
-		}
-	}
-	return 0;
-}
+#include <stdio.h>
 
+int main() {
+    int octal, decimal = 0, base = 1;
+    while (scanf("%d", &octal) != EOF)
+    {
+        if (octal == 0)
+        {
+            return 0;
+        }
+    
+        while (octal != 0) 
+        {
+            int digit = octal % 10;
+            decimal += digit * base;
+            base *= 8;
+            octal /= 10;
+        } printf("%d\n", decimal); octal = 0; decimal = 0; base = 1;
+    }
+    return 0;
+}
